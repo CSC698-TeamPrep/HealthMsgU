0c40e4cc433f6c064de4ff3dcec49e61153ef5e5
N-Rodrigues <32182527+N-Rodrigues@users.noreply.github.com>
2018-04-17 12:19:10 -0700

    removed extra comments

--- timeZoneTweetsWorkingPythonCode.py
+++ timeZoneTweetsWorkingPythonCode.py
@@ -1,37 +1,10 @@
 # TO USE TO GET TIME ZONES
 # THIS CODE WORKS WAHOO
 
-
-
-# print("hello world")
-#
-# import tweepy
-#
-# auth = tweepy.OAuthHandler("iHTvDAptJAx3PwfYfw00Qa6sX", "6Gp8kYPR3kULcPPdW3k1gYopQEi4NBdSfFPP8ifHCE0gwkQWCX")
-# auth.set_access_token("938932912870658048-ICcR2mOBhIvNtIQmMSVdtuQbUsCxIXL", "QU7ArtcKW1aEQYcf0HlMUpS7m07UtQmUNl42OvAJyZ5SH")
-#
-# api = tweepy.API(auth)
-#
-# public_tweets = api.home_timeline()
-# for tweet in public_tweets:
-#    print(tweet.text)
-#    #*Task two:* You've got Python and Tweepy running on your machine -- now write a Python script that scrapes
-#    # Twitter for posts about a query that you provide!
-#    #  If you can, have your script output a couple of datums for the posts you scrape -- the username of the poster, the date, etc.
-#
-
-
-
-
-
-# to make a better search
 #asking what the user wants to search for
 wordSearch = input("What's input do you want to search for? ")
 print("This is your input: " + wordSearch )
 
-#word="dum"
-#if wordSearch in word:
- #  print('Success!')
 
 import tweepy
 
@@ -43,7 +16,7 @@ api = tweepy.API(auth)
 
 #items say number of tweets looked at
 for tweet in tweepy.Cursor(api.search,q=wordSearch).items(20):
-    # just print(tweet.text) for the results (tweet.author or dae)
+    # just print(tweet.text) for the results (tweet.author or date)
     # just print(tweet) for all the results
 
     x=tweet.author
@@ -51,4 +24,4 @@ for tweet in tweepy.Cursor(api.search,q=wordSearch).items(20):
 
     print(y)
     #print(tweet.text)
-    # x.geo_enabled works0
+    # x.geo_enabled works
