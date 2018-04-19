002b2c018aea25fe8b170fff40bc3789b7321ba3
N-Rodrigues <32182527+N-Rodrigues@users.noreply.github.com>
2018-04-19 11:00:31 -0700

    created def

--- timeZoneBarGraph.py
+++ timeZoneBarGraph.py
@@ -17,54 +17,57 @@ wordSearch = input("What's input do you want to search for? ")
 print("This is your input: " + wordSearch )
 
 #items say number of tweets looked at
-mylist = []
-for tweet in tweepy.Cursor(api.search,q=wordSearch).items(10):
-    # just print(tweet.text) for the results (tweet.author or date)
-    # just print(tweet) for all the results
+def FlaskBarGraph():
+    mylist = []
+    for tweet in tweepy.Cursor(api.search,q=wordSearch).items(10):
+        # just print(tweet.text) for the results (tweet.author or date)
+        # just print(tweet) for all the results
 
-    x=tweet.author
-    y=x.time_zone
-    mylist.append(str(y))
-    #print(mylist)
-    #print(tweet.text)
-    # x.geo_enabled works
+        x=tweet.author
+        y=x.time_zone
+        mylist.append(str(y))
+        #print(mylist)
+        #print(tweet.text)
+        # x.geo_enabled works
 
-    # this bit of code is to get a frequency of mylist
-    from collections import Counter
-    counts = Counter(mylist)
-    #counts= dictionary
-    print(counts)
-    #this is what is printed out => Counter({'None': 7, 'Central Time (US & Canada)': 1, 'America/Caracas': 1, 'Arizona': 1})
+        # this bit of code is to get a frequency of mylist
+        from collections import Counter
+        counts = Counter(mylist)
+        #counts= dictionary
+        print(counts)
+        #this is what is printed out => Counter({'None': 7, 'Central Time (US & Canada)': 1, 'America/Caracas': 1, 'Arizona': 1})
 
-    #count.get is how you get values from the key
-    default_value=0 #the default_value gives you a 0 for the time zone,
-                    # as sometimes a timezone isn't there and then its value will be none and this mess up the code
-    pt=counts.get("Pacific Time (US & Canada)",default_value)
-    print(pt)
-    et=counts.get("Eastern Time (US & Canada)",default_value)
-    print(et)
-    mt=counts.get("Mountain Time (US & Canada)",default_value)
-    print(mt)
-    az=counts.get("Alaska",default_value)
-    print(az)
-    hz=counts.get("Hawaii",default_value)
-    print(hz)
-    ct = counts.get("Central Time (US & Canada)", default_value)
-    print(ct)
-    nz =(10-(pt+et+mt+az+hz+ct))  #to show the other time zone tweets and no timezone tweets
+        #count.get is how you get values from the key
+        default_value=0 #the default_value gives you a 0 for the time zone,
+                        # as sometimes a timezone isn't there and then its value will be none and this mess up the code
+        pt=counts.get("Pacific Time (US & Canada)",default_value)
+        print(pt)
+        et=counts.get("Eastern Time (US & Canada)",default_value)
+        print(et)
+        mt=counts.get("Mountain Time (US & Canada)",default_value)
+        print(mt)
+        az=counts.get("Alaska",default_value)
+        print(az)
+        hz=counts.get("Hawaii",default_value)
+        print(hz)
+        ct = counts.get("Central Time (US & Canada)", default_value)
+        print(ct)
+        nz =(10-(pt+et+mt+az+hz+ct))  #to show the other time zone tweets and no timezone tweets
 
-fig = plt.figure
+    fig = plt.figure
 
-objects = ('Eastern  ', 'Pacific  ', 'Mountain  ', 'Alaska', 'Central  ', 'Hawaii','Other')
-y_pos = np.arange(len(objects))
-# to give each bar its value
-performance = [et,pt, mt, az, ct, hz,nz]
+    objects = ('Eastern  ', 'Pacific  ', 'Mountain  ', 'Alaska', 'Central  ', 'Hawaii','Other')
+    y_pos = np.arange(len(objects))
+    # to give each bar its value
+    performance = [et,pt, mt, az, ct, hz,nz]
 
-plt.bar(y_pos, performance, align='center', alpha=0.5)
-plt.xticks(y_pos, objects)
-plt.ylabel('Number of Tweets')
-plt.title('Time Zones')
+    plt.bar(y_pos, performance, align='center', alpha=0.5)
+    plt.xticks(y_pos, objects)
+    plt.ylabel('Number of Tweets')
+    plt.title('Time Zones')
 
-#plt.show()  to display on pycharm
-#savfig= saves graph as png
-plt.savefig('ah.png')
+    #plt.show()  to display on pycharm
+    #savfig= saves graph as png
+    plt.savefig('ah.png')
+
+FlaskBarGraph()
