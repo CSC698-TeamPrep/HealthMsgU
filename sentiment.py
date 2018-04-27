6ccded45c07ab6bc9dd67bd588f5a2374bb61160
Emily Fryer <efryer@mail.sfsu.edu>
2018-04-27 13:56:05 -0700

    Updated this branch to work on data display issues via cache-busting

--- sentiment.py
+++ sentiment.py
@@ -2,6 +2,8 @@ import re
 import tweepy
 from tweepy import OAuthHandler
 from textblob import TextBlob
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 #Most code borrowed from https://www.geeksforgeeks.org/twitter-sentiment-analysis-using-python/
 #Slightly modified, original borrowed code actually doesn't compile without slight modifications
@@ -96,38 +98,3 @@ class TwitterClient(object):
         except tweepy.TweepError as e:
             # print error (if any)
             print("Error : " + str(e))
-
-
-def main():
-    # creating object of TwitterClient Class
-    api = TwitterClient()
-    # calling function to get tweets
-    #searchterm = input("Enter query: ")
-    tweets = api.get_tweets(query=searchterm, count=10)
-
-    # picking positive tweets from tweets
-    ptweets = [tweet for tweet in tweets if tweet['sentiment'] == 'positive']
-    # percentage of positive tweets
-    print("Positive tweets percentage: {} %".format(100 * len(ptweets) / len(tweets)))
-    # picking negative tweets from tweets
-    ntweets = [tweet for tweet in tweets if tweet['sentiment'] == 'negative']
-    # percentage of negative tweets
-    print("Negative tweets percentage: {} %".format(100 * len(ntweets) / len(tweets)))
-    # percentage of neutral tweets
-    #leftoverTweets = tweets - ntweets - ptweets
-    print("Neutral tweets percentage: {} % \ ".format(100 * (len(tweets) - len(ntweets) - len(ptweets)) / len(tweets)))
-
-    # printing first 5 positive tweets
-    print("\n\nPositive tweets:")
-    for tweet in ptweets[:10]:
-        print(tweet['text'])
-
-    # printing first 5 negative tweets
-    print("\n\nNegative tweets:")
-    for tweet in ntweets[:10]:
-        print(tweet['text'])
-
-
-if __name__ == "__main__":
-    # calling main function
-    main()
